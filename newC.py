996516fed9760ce3b7a0db4078d607689ca4b192
0Nguyen0Cong0Tuan0 <119025574+0Nguyen0Cong0Tuan0@users.noreply.github.com>
2023-11-13 23:07:44 +0700

    Update_3

--- newC.py
+++ newC.py
@@ -2,7 +2,23 @@ import socket
 
 HEADER = 1024
 
-def send_email(From, To, Cc, Bcc, subject, content):
+def getEmailTo(mails_address_to):
+    To = input("To: ")
+    mails_address_to.append(To)
+
+def getEmailCc(mails_address_cc):
+    Cc = input("CC: ")
+    Cc = Cc.split(',')
+    Cc = [email.strip() for email in Cc]
+    mails_address_cc.extend(Cc)
+    
+def getEmailBcc(mails_address_bcc):
+    Bcc = input("BCC: ")
+    Bcc = Bcc.split(',')
+    Bcc = [email.strip() for email in Bcc]
+    mails_address_bcc.extend(Bcc)
+
+def send_email_to(From, To, subject, content):
     with socket.create_connection(("127.0.0.1", 2225)) as server_socket:
         response = server_socket.recv(HEADER).decode()
         if not response.startswith('220'):
@@ -28,30 +44,112 @@ def send_email(From, To, Cc, Bcc, subject, content):
         if not response.startswith('354'):
             raise Exception(f"Error sending data: {response}")
         
-        email_data = f"To: {To}\r\nFrom: {From}\r\nCC: {Cc}\r\nBCC: {Bcc}\r\nSubject: {subject}\r\n\r\n{content}\r\n"
+        email_data = f"To: {To}\r\nFrom: {From}\r\nSubject: {subject}\r\n\r\n{content}\r\n"
         server_socket.sendall(f"{email_data}\r\n.\r\n".encode())
         response = server_socket.recv(HEADER).decode()
         if not response.startswith('250'):
             raise Exception(f"Error sending email: {response}")
 
-def getUserInfo():
-    Username = "Nguyen Cong Tuan <nctuan22@clc.fitus.edu.vn>"
-    Password = "123456"
-    MailServer = "127.0.0.1"
-    SMTP = "2225"
-    POP3 = "3335"
-    AuToload = 10
+def send_email_cc(From, Cc, mails_address_cc, subject, content):
+    with socket.create_connection(("127.0.0.1", 2225)) as server_socket:
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('220'):
+            raise Exception(f"Error connecting To server: {response}")
+        
+        server_socket.send("EHLO [127.0.0.1]\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending EHLO: {response}")
+        
+        server_socket.send(f"MAIL FROM:<{From}>\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending mail address: {response}")
+        
+        server_socket.send(f"RCPT TO:<{Cc}>\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending mail address: {response}")
+        
+        server_socket.send("DATA\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('354'):
+            raise Exception(f"Error sending data: {response}")
+        
+        email_data = f"CC: {mails_address_cc}\r\nFrom: {From}\r\nSubject: {subject}\r\n\r\n{content}\r\n"
+        server_socket.sendall(f"{email_data}\r\n.\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending email: {response}")
 
-def main():
+def send_email_bcc(From, Bcc, subject, content):
+    with socket.create_connection(("127.0.0.1", 2225)) as server_socket:
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('220'):
+            raise Exception(f"Error connecting To server: {response}")
+        
+        server_socket.send("EHLO [127.0.0.1]\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending EHLO: {response}")
+        
+        server_socket.send(f"MAIL FROM:<{From}>\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending mail address: {response}")
+        
+        server_socket.send(f"RCPT TO:<{Bcc}>\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending mail address: {response}")
+        
+        server_socket.send("DATA\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('354'):
+            raise Exception(f"Error sending data: {response}")
+        
+        email_data = f"BCC: {Bcc}\r\nFrom: {From}\r\nSubject: {subject}\r\n\r\n{content}\r\n"
+        server_socket.sendall(f"{email_data}\r\n.\r\n".encode())
+        response = server_socket.recv(HEADER).decode()
+        if not response.startswith('250'):
+            raise Exception(f"Error sending email: {response}")
+
+def run_send_mail_program():
+    mails_address_to = []
+    mails_address_cc = []
+    mails_address_bcc = []
+
     From = input("From: ")
-    To = input("To: ")
-    Cc = input("CC: ")
-    Bcc = input("BCC: ")
+
+    getEmailTo(mails_address_to)
+    getEmailCc(mails_address_cc)
+    getEmailBcc(mails_address_bcc)
+   
     subject = input("Subject: ")
-    content = input("content: ")
+    content = input("Content: ")
     #attach_files = input("Attack files (comma-separated): ").split(',')
 
-    send_email(From, To, Cc, Bcc, subject, content)
+    for To in mails_address_to:
+        send_email_to(From, To, subject, content)
+
+    for Cc in mails_address_cc:
+        send_email_cc(From, Cc, subject, content)
+
+    for Bcc in mails_address_bcc:
+        send_email_bcc(From, Bcc, subject, content)
+
+
+# def getUserInfo():
+#     Username = "Nguyen Cong Tuan <nctuan22@clc.fitus.edu.vn>"
+#     Password = "123456"
+#     MailServer = "127.0.0.1"
+#     SMTP = "2225"
+#     POP3 = "3335"
+#     AuToload = 10
+
+def main():
+    run_send_mail_program()
+    
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,12 +1,24 @@
-emails_input = input("Enter emails separated by commas: ")
+# emails_input = input("Enter emails separated by commas: ")
 
-# Split the input string into a list of emails
-email_list = emails_input.split(',')
+# # Split the input string into a list of emails
+# email_list = emails_input.split(',')
 
-# Remove any leading or trailing whitespaces from each email
-email_list = [email.strip() for email in email_list]
+# # Remove any leading or trailing whitespaces from each email
+# email_list = [email.strip() for email in email_list]
 
-# Convert the list to a tuple if needed
-email_tuple = tuple(email_list)
+# # Convert the list to a tuple if needed
+# email_tuple = tuple(email_list)
 
-print("Email Tuple:", email_tuple)
+# print("Email Tuple:", email_tuple)
+
+mails_address = []
+emails = "1@gmail.com"
+mails_address.append(emails)
+
+email_input = "nguyenvanA@gmail.com, nguyenVanB@gmail.com, 2@gmail.com"
+email_input = email_input.split(',')
+email_input = [email.strip() for email in email_input]
+mails_address.extend(email_input)
+
+for mail in mails_address:
+    print(mail)
